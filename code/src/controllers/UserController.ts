5c9533838f456388e7864990f327c940dea85d43
Sheen <sheen@symph.co>
2021-08-21 18:35:52 +0800

    [Adjustment][Sheen] set up app engine deployment

--- code/src/controllers/UserController.ts
+++ code/src/controllers/UserController.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const userRouter = Router();
 
 userRouter.get('/create', (req, res) => {
-    console.log('HELLO I AM UNDERWATER');
+    res.send('HELLO I AM UNDERWATER');
 });
 
 export default userRouter;
