e92b3b11705e830e79e99d959535a8ef8e51962c
Sheen <sheen@symph.co>
2021-08-21 18:57:26 +0800

    [Test][Sheen] test

--- code/src/controllers/UserController.ts
+++ code/src/controllers/UserController.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const userRouter = Router();
 
 userRouter.get('/create', (req, res) => {
-    res.send('HELLO I AM UNDERWATER');
+    res.send('HELLO I AM UNDERWATER, HELP ME PLZ');
 });
 
 export default userRouter;
