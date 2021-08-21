d1cae34e834361addf299a889c2c16668827faf3
Sheen <sheen@symph.co>
2021-08-21 19:12:39 +0800

    [Test][Sheen] cloud build test 1

--- code/src/controllers/UserController.ts
+++ code/src/controllers/UserController.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const userRouter = Router();
 
 userRouter.get('/create', (req, res) => {
-    res.send('HELLO, THIS IS NOT A TEST.');
+    res.send('HELLO, THIS IS NOT A TEST. DIS IS DA PURGE');
 });
 
 export default userRouter;
