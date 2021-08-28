31b51a5451dd0122559975930a58341537894d7f
Sheen <sheen@symph.co>
2021-08-28 20:21:06 +0800

    [Fix][Sheen] resolved startup error

--- code/src/config/index.ts
+++ code/src/config/index.ts
@@ -7,15 +7,16 @@ dotenv.config({
 const isProduction = process.env.NODE_ENV === 'production';
 
 const buildConfig = () => ({
-    dbConnectionName: process.env.DATABASE_NAME,
+    dbConnectionName: process.env.DATABASE_CONNECTION_NAME,
+    dbDatabaseName: process.env.DATABASE_NAME,
     dbIpAddress: isProduction
         ? process.env.DATABASE_IP_ADDRESS
         : process.env.DEV_DATABASE_IP_ADDRESS,
-    dbUser: process.env.DATABASE_USER,
     dbPassword: process.env.DATABASE_PASSWORD,
     dbPort: isProduction
         ? parseInt(process.env.DATABASE_PORT as string)
         : parseInt(process.env.DEV_DATABASE_PORT as string),
+    dbUser: process.env.DATABASE_USER,
 });
 
 export default buildConfig();
--- code/src/services/db/DatabaseService.ts
+++ code/src/services/db/DatabaseService.ts
@@ -3,18 +3,27 @@ import { promisify } from 'util';
 
 import config from '../../config';
 
-console.log('CONFIG IS', config);
+console.log('NODE ENV IS', process.env.NODE_ENV, 'WITH CONFIGS', config);
 
 export default class DatabaseService {
     private dbConnection;
     private promisfiedQuery: any;
     constructor() {
+        const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+        const additionalConfig =
+            process.env.NODE_ENV === 'production'
+                ? {
+                      socketPath: `${dbSocketPath}/${config.dbConnectionName}}`,
+                  }
+                : {
+                      host: config.dbIpAddress,
+                      port: config.dbPort,
+                  };
         this.dbConnection = mysql.createPool({
             user: config.dbUser,
             password: config.dbPassword,
-            database: config.dbConnectionName,
-            host: config.dbIpAddress,
-            port: config.dbPort,
+            database: config.dbDatabaseName,
+            ...additionalConfig,
         });
         this.promisfiedQuery = promisify(this.dbConnection.query).bind(
             this.dbConnection
